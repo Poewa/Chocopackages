3eed69e85e055e5504254f70810e1de2bd2659ef
Rasmus Brandt <rasmusbrandt92@gmail.com>
2021-03-09 22:53:47 +0100

    first commit

---
+++ pictor-selector/tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+﻿$ErrorActionPreference = 'Stop';
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://pictoselector.sclera.be/setup_complete.exe'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  softwareName  = 'picto-select*'
+  checksum      = 'F15B1786697F4B8CCDEC41C80BA238C8CB78A855E0A27BCB75DEFE730A9EFD9D'
+  checksumType  = 'sha256'
+  silentArgs    = "/VERYSILENT"
+  validExitCodes= @(0, 3010, 1641)
+}
+
+
+Install-ChocolateyPackage @packageArgs
